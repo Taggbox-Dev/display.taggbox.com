a6305735434adf864a0fce85ecf9a88145844077
sudarshan bairagi <sudarshan.bairagi@taggbox.com>
2023-03-01 11:40:35 +0530

    update

---
+++ precache-manifest.c009340a05199466698c9498f4bdc78e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4c7df44b89c29e7d34001c6ff15a10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "443c230f97d6334e80eb",
+    "url": "/static/css/2.54a0b3a4.chunk.css"
+  },
+  {
+    "revision": "8feef6faec631d9ee6ce",
+    "url": "/static/css/main.dcd75785.chunk.css"
+  },
+  {
+    "revision": "443c230f97d6334e80eb",
+    "url": "/static/js/2.2af4ae2f.chunk.js"
+  },
+  {
+    "revision": "19858d1765d2b796fa898e4c63fa6469",
+    "url": "/static/js/2.2af4ae2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "280d3ea00b2befc008ca",
+    "url": "/static/js/3.f296469f.chunk.js"
+  },
+  {
+    "revision": "8feef6faec631d9ee6ce",
+    "url": "/static/js/main.a22ad12f.chunk.js"
+  },
+  {
+    "revision": "d10a3dbd36feb4bdbdbe",
+    "url": "/static/js/runtime-main.4bd0c1f9.js"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/static/media/OpenSans-Regular.629a55a7.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.90fdecd10be0ff34b125269f1bb14bd8.js"
+  "/precache-manifest.c009340a05199466698c9498f4bdc78e.js"
 );
 
 self.addEventListener('message', (event) => {
