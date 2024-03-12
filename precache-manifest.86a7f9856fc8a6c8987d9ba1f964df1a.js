f592af62fb3222e85c718106b7765347010effb2
sudarshan bairagi <sudarshan.bairagi@taggbox.com>
2024-03-12 18:40:15 +0530

    update

---
+++ precache-manifest.86a7f9856fc8a6c8987d9ba1f964df1a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e363fb99a9804c4917c1a5fc09fac4ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbb177b86ae8ec104221",
+    "url": "/static/css/2.0063d13a.chunk.css"
+  },
+  {
+    "revision": "8903e7e6964361096cb8",
+    "url": "/static/css/main.5697cf47.chunk.css"
+  },
+  {
+    "revision": "fbb177b86ae8ec104221",
+    "url": "/static/js/2.baae0fd5.chunk.js"
+  },
+  {
+    "revision": "1503777cb1601b711802df3fbfdf08da",
+    "url": "/static/js/2.baae0fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab48c095d687174c36a6",
+    "url": "/static/js/3.ee372cae.chunk.js"
+  },
+  {
+    "revision": "04fc4a3d3bb6d7672f26",
+    "url": "/static/js/4.ebe0a489.chunk.js"
+  },
+  {
+    "revision": "8903e7e6964361096cb8",
+    "url": "/static/js/main.6da3c2cc.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.6da3c2cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b619e7744623fa94017f",
+    "url": "/static/js/runtime-main.52c1c7c0.js"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/static/media/OpenSans-Regular.629a55a7.ttf"
+  }
+]);
