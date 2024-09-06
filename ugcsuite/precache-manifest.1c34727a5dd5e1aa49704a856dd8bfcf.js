c72340b8019c58c921ac4669a01fdfe65cd40fb3
sudarshan <sudarshan.b@taggbox.com>
2024-09-07 00:57:53 +0530

    update ugc suite

---
+++ ugcsuite/precache-manifest.1c34727a5dd5e1aa49704a856dd8bfcf.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3daaa74fe6a42f148a393e17766fc831",
+    "url": "https://display.taggbox.com/ugcsuite/index.html"
+  },
+  {
+    "revision": "bc4fb4d2c341fef92108",
+    "url": "https://display.taggbox.com/ugcsuite/static/css/2.c1d165e6.chunk.css"
+  },
+  {
+    "revision": "e08734d2009e6701254c",
+    "url": "https://display.taggbox.com/ugcsuite/static/css/main.f8798621.chunk.css"
+  },
+  {
+    "revision": "bc4fb4d2c341fef92108",
+    "url": "https://display.taggbox.com/ugcsuite/static/js/2.3493ece4.chunk.js"
+  },
+  {
+    "revision": "1503777cb1601b711802df3fbfdf08da",
+    "url": "https://display.taggbox.com/ugcsuite/static/js/2.3493ece4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef88a2ed358d3750d7e9",
+    "url": "https://display.taggbox.com/ugcsuite/static/js/3.42fdb9c0.chunk.js"
+  },
+  {
+    "revision": "9af9911842a166a828a0",
+    "url": "https://display.taggbox.com/ugcsuite/static/js/4.ef02be2c.chunk.js"
+  },
+  {
+    "revision": "e08734d2009e6701254c",
+    "url": "https://display.taggbox.com/ugcsuite/static/js/main.8b1a3214.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "https://display.taggbox.com/ugcsuite/static/js/main.8b1a3214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c7c8ff11dcb11958ba",
+    "url": "https://display.taggbox.com/ugcsuite/static/js/runtime-main.0ba53019.js"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "https://display.taggbox.com/ugcsuite/static/media/OpenSans-Regular.629a55a7.ttf"
+  }
+]);
